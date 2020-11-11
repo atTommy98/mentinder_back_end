f3cf90cd1c8d2346587a10bad28d73545bacdc1f
= <patrickjosephfleming@gmail.com>
2020-11-11 11:01:14 +0000

    cors added

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mentorsAndBootcampers = require("./routes/mentorsAndBootcampers");
+const cors = require("cors");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
